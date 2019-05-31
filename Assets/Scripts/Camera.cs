405f421bfc4bd72cd522ce78da1bff820323012c
hastgt <hastgt@icloud.com>
2019-05-31 15:22:04 +0200

    movement

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Camera : MonoBehaviour
+{
+    void Update()
+    {
+       
+        transform.localPosition = new Vector3(0, 5, 3);
+
+    }
+}
