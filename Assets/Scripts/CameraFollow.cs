90424239c9b7f1e3a450b3d6610641c6fcc15dc9
Ivan <mrpalich@gmail.com>
2019-06-01 19:06:51 +0300

    Переписал то, что написал ты. Добавил комментарии с пояснениями.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -1,8 +1,6 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class camerafollow : MonoBehaviour
+public class CameraFollow : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
