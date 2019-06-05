605cc236d3a1f3b60ed36f166937061e94576900
hastgt <hastgt@icloud.com>
2019-06-05 16:46:28 +0200

    Revert "idk"
    This reverts commit 2a5ba2a0820589d047ee7a5fdd1174c2ab643da7.

--- Assets/Scripts/DropController.cs
+++ Assets/Scripts/DropController.cs
@@ -8,23 +8,16 @@ public class DropController : MonoBehaviour
     private Rigidbody rb = null;
     private float x;
     private Vector3 direction = Vector3.zero;
-    private Camera mainCamera;
-    
 
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
-        mainCamera = FindObjectOfType<Camera>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        x = Input.GetAxis("Horizontal");
-        direction = new Vector3(x, 0, 0);
-        Move(direction);
-        
         //GetKey используется только на устройствах с клавиатурой. Для унифицированного ввода используй Axis в Unity Input.
         //Изменение position у transform и rigidbody равна телепортации объекта. Так лучше не делать за редкими исключениями.
 
@@ -37,7 +30,9 @@ public class DropController : MonoBehaviour
         //{
         //    transform.position += Vector3.right * speed * Time.deltaTime;
         //}
-
+        x = Input.GetAxis("Horizontal");
+        direction = new Vector3(x, 0, 0);
+        Move(direction);
     }
 
     //У компонента Rigidbody выбрать FreezeRotation во всех направлениях. Freeze Position по Y, Z.
@@ -46,6 +41,4 @@ public class DropController : MonoBehaviour
         //rb.velocity = (new Vector3(direction.x * speed, 0, 0));
         rb.MovePosition(transform.position + direction * speed * Time.deltaTime);
     }
-
-    
 }
