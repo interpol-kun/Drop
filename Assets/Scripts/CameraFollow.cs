deee757ffaea79647f9e637013417d882fcca95b
Ivan <mrpalich@gmail.com>
2019-06-10 17:59:27 +0300

    Новая камера
    Ортографическая, изменил следование камеры за игроком.

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,17 +7,22 @@ public class CameraFollow : MonoBehaviour
     public Transform target;
 
     public float smoothSpeed = 0.125f;
+
+    //(0, 14.5, 0)
     public Vector3 offset;
 
     private Vector3 desiredPosition, smoothedPosition;
 
+    private void Start()
+    {
+        transform.Rotate(new Vector3(90, 0, 0));
+    }
     void LateUpdate()
     {
-        desiredPosition  = transform.position = target.position + offset;
+        desiredPosition  = transform.position = new Vector3(0, target.position.y, 0) + offset;
         smoothedPosition  = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
         transform.position = smoothedPosition;
 
-        transform.LookAt(target);
     }
 
 }
